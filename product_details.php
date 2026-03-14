44f0cf45cab530b9bc7f6be66e4eaa2cfbbdb5b8
cp308 <phamchiphuong2004@gmail.com>
2026-03-14 18:58:36 +0700

    product_details.php

---
+++ product_details.php
@@ -0,0 +1,12 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Details</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/product_details.css">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    <br>
