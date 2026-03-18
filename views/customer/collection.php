438db5d4f6818c61be93951f517f9521360ae655
cp308 <phamchiphuong2004@gmail.com>
2026-03-18 14:37:44 +0700

    collection.php

--- views/customer/collection.php
+++ views/customer/collection.php
@@ -15,4 +15,22 @@ if ($result) {
     }
 }
 $conn->close();
+ // Xác định collection được chọn
+$selected = isset($_GET['c']) ? $_GET['c'] : 'all';
  
+// Lấy sản phẩm thuộc collection nếu đã chọn collection cụ thể
+$products = [];
+if (is_numeric($selected) && isset($collections[$selected])) {
+    $conn = new mysqli('localhost', 'root_user', 'admin123', 'ql_bakery');
+    $conn->set_charset('utf8');
+    $stmt = $conn->prepare("SELECT * FROM products WHERE collection_id = ? AND stock > 0");
+    $stmt->bind_param("i", $collections[$selected]['id']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+        $products[] = $row;
+    }
+    $stmt->close();
+    $conn->close();
+}
+?>
