f985bf4553f658ebdd2460aa2077567608fa425f
Pham Khanh Linh <khanhling212@gmail.com>
2026-03-27 16:16:40 +0700

    Update noti.php

--- views/customer/noti.php
+++ views/customer/noti.php
@@ -51,3 +51,38 @@ $stmt->close();
     </style>
 </head>
 <body>
+    <?php include '../../includes/header.php'; ?>
+    <div class="container">
+        <h2>Notifications</h2>
+        <?php if (empty($notifications)): ?>
+            <div class="empty">You have no notifications.</div>
+        <?php else: ?>
+        <table>
+            <tr>
+                <th>Type</th>
+                <th>Message</th>
+                <th>Product</th>
+                <th>Order</th>
+                <th>Date</th>
+            </tr>
+            <?php foreach ($notifications as $noti): ?>
+            <tr>
+                <td class="noti-type-<?php echo htmlspecialchars($noti['type']); ?>">
+                    <?php echo ucfirst(str_replace('_', ' ', $noti['type'])); ?>
+                </td>
+                <td><?php echo htmlspecialchars($noti['message']); ?></td>
+                <td>
+                    <?php echo $noti['product_name'] ? htmlspecialchars($noti['product_name']) : '-'; ?>
+                </td>
+                <td>
+                    <?php echo $noti['order_number'] ? 'Order #' . htmlspecialchars($noti['order_number']) : '-'; ?>
+                </td>
+                <td class="noti-date"><?php echo date('d/m/Y H:i', strtotime($noti['created_at'])); ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <?php endif; ?>
+    </div>
+    <?php include '../../includes/footer.php'; ?>
+</body>
+</html>
